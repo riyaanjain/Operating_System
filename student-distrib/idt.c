8fbc74dde48e908d4d295aa64d863a489081fcfd
pranavkommaraju <pranavkv13@gmail.com>
2024-03-08 16:40:32 -0600

    created exceptions file and added more to idt

--- student-distrib/idt.c
+++ student-distrib/idt.c
@@ -1,41 +1,41 @@
 #include "idt.h"
 
 void init_idt() {
-    // offset (16 bits) - offset_31_16
-    // p (1 bit) - present
-    // dpl (2 bits) - dpl
-    // 0 (1 bit) - reserved0
-    // gatetype (4 bits) - {size, reserved1, reserved2, reserved3}
-    // reserved (8 bits) - reserved4
-    // segselector (16 bits) - seg_selector
-    // offset (16 bits) - offset_15_00
-
-    // dpl is 0 for interrupts and 3 for sys calls
-    // present set to 1 for valid descriptors (except 15)
-    // gate type:
-        // 0b0101 or 0x5: Task Gate, note that in this case, the Offset value is unused and should be set to zero.
-        // 0b0110 or 0x6: 16-bit Interrupt Gate
-        // 0b0111 or 0x7: 16-bit Trap Gate
-        // 0b1110 or 0xE: 32-bit Interrupt Gate
-        // 0b1111 or 0xF: 32-bit Trap Gate
-    // segselector: {index, ti, rpl}
-        // Index: Bits 3-15 of the Index of the GDT or LDT entry referenced by the selector. Since Segment Descriptors are 8 bytes in length, the value of Index is never unaligned and contains all zeros in the lowest 3 bits.
-        // TI: Specifies which descriptor table to use. If clear (0) then the GDT is used, if set (1) then the current LDT is used.
-        // RPL: same as dpl for selector
-
     int i;
+
     for (i = 0; i < NUM_VEC; i++) {
-        idt[i].reserved0 = 0;
-        if (i < 20 && i != 15) {
+        // set present bit
+        if ((i < 21 && i != 15) || i == SYSTEM_CALLS || i == KEYBOARD_CALLS || i == RTC_CALLS) {
             idt[i].present = 1;
         } else {
             idt[i].present = 0;
         }
-        idt[i].size = 32;
+
+        // set dpl to 0 for exceptions and 3 for system calls
+        if (i == SYSTEM_CALLS) {
+            idt[i].dpl = 3;
+        } else {
+            idt[i].dpl = 0;
+        }
+
+        // set reserved0 to 0
+        idt[i].reserved0 = 0;
+
+        // set gate type (reseerved3 is 1 for keyboard and rtc)
+        idt[i].size = 1;
         idt[i].reserved1 = 1;
         idt[i].reserved2 = 1;
-        idt[i].reserved3 = 0;
-        idt[i].dpl = 0;
+        if (i == KEYBOARD_CALLS || i == RTC_CALLS) {
+            idt[i].reserved3 = 1;
+        } else {
+            idt[i].reserved3 = 0;
+        }
+
+        // set reserved4 to 0
+        idt[i].reserved4 = 0;
+        
+        // set seg selector to kernel code segment
+        idt[i].seg_selector = KERNEL_CS;
     }
 
     
@@ -58,5 +58,10 @@ void init_idt() {
     SET_IDT_ENTRY(idt[17], ); // Alignment Check
     SET_IDT_ENTRY(idt[18], ); // Machine Check
     SET_IDT_ENTRY(idt[19], ); // SIMD Floating-Point Exception
+    SET_IDT_ENTRY(idt[20], ); // Virtualization Exception
+    SET_IDT_ENTRY(idt[SYSTEM_CALLS], ); // System Calls
+    SET_IDT_ENTRY(idt[KEYBOARD_CALLS], ) // Keyboard Calls
+    SET_IDT_ENTRY(idt[RTC_CALLS], ); // RTC Calls
 
+    lidt(idt_desc_ptr);
 }
--- student-distrib/idt.h
+++ student-distrib/idt.h
@@ -2,8 +2,13 @@
 #define _idt_H
 
 #include "x86_desc.h"
+#include "exceptions.h"
 
 #ifndef ASM
+
+#define SYSTEM_CALLS    0x80
+#define KEYBOARD_CALLS  33
+#define RTC_CALLS       40
 
 void init_idt();
 
