0202a1fd50ab39338321d7effa9ea21496fc1371
pranavkommaraju <pranavkv13@gmail.com>
2024-03-11 23:06:58 -0500

    added tests

--- student-distrib/exception_handler.c
+++ student-distrib/exception_handler.c
@@ -1,20 +1,17 @@
 #include "exception_handler.h"
 
 void exc_handler(uint32_t id, struct registers regs, uint32_t error_code) {
+    // clear the screen
     clear();
-    
-    printf("\033[1;44m"); // Set background color to blue
+
     printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-    printf("\033[1;37m"); // Set text color to white
     printf("\t\t\tException Synopsis:\n\n");
-    printf("\033[1;34m"); // Set text color to blue
 
     if (id < NUM_EXCEPTIONS) {
         printf("\t\t\tException Raised: %s\n", os_exceptions[id]);
         printf("\t\t\tEntry Number: %d\n\n", id);
     }
 
-    printf("\033[1;37m"); // Set text color to white
 
     printf("\t\t\tTechnical Error Code Data:\n\n");
     printf("\t\t\tError Code: %d\n\n", error_code);
@@ -29,6 +26,5 @@ void exc_handler(uint32_t id, struct registers regs, uint32_t error_code) {
     printf("\t\t\tESP: %d\n", regs.esp);
     printf("\t\t\tEBP: %d\n", regs.ebp);
 
-    printf("\033[0m"); // Reset color
+    while(1);
 }
-
--- student-distrib/idt.c
+++ student-distrib/idt.c
@@ -21,7 +21,7 @@ void init_idt() {
         // set reserved0 to 0
         idt[i].reserved0 = 0;
 
-        // set gate type (reseerved3 is 1 for keyboard and rtc)
+        // set gate type (reserved3 is 1 for keyboard and rtc)
         idt[i].size = 1;
         idt[i].reserved1 = 1;
         idt[i].reserved2 = 1;
@@ -39,28 +39,28 @@ void init_idt() {
     }
 
     
-    // SET_IDT_ENTRY(idt[0], division_error); // Division Error
-    // SET_IDT_ENTRY(idt[1], debug); // Debug
-    // SET_IDT_ENTRY(idt[2], non_maskable_interrupt); // Non-maskable Interrupt
-    // SET_IDT_ENTRY(idt[3], breakpoint); // Breakpoint
-    // SET_IDT_ENTRY(idt[4], overflow); // Overflow
-    // SET_IDT_ENTRY(idt[5], bound_range_exceeded); // Bound Range Exceeded
-    // SET_IDT_ENTRY(idt[6], invalid_opcode); // Invalid Opcode
-    // SET_IDT_ENTRY(idt[7], device_not_available); // Device Not Available
-    // SET_IDT_ENTRY(idt[8], double_fault); // Double Fault
-    // SET_IDT_ENTRY(idt[9], coprocessor_segment_overrun); // Coprocessor Segment Overrun
-    // SET_IDT_ENTRY(idt[10], invalid_tss); // Invalid TSS
-    // SET_IDT_ENTRY(idt[11], segment_not_present); // Segment Not Present
-    // SET_IDT_ENTRY(idt[12], stack_segment_fault); // Stack-Segment Fault
-    // SET_IDT_ENTRY(idt[13], general_protection_fault); // General Protection Fault
-    // SET_IDT_ENTRY(idt[14], page_fault); // Page Fault
-    // SET_IDT_ENTRY(idt[16], asm_floating_point_exception); // x87 Floating-Point Exception
-    // SET_IDT_ENTRY(idt[17], alignment_check); // Alignment Check
-    // SET_IDT_ENTRY(idt[18], machine_check); // Machine Check
-    // SET_IDT_ENTRY(idt[19], simd_floating_point_exception); // SIMD Floating-Point Exception
-    // SET_IDT_ENTRY(idt[SYSTEM_CALLS], system_call); // System Calls
-    // // SET_IDT_ENTRY(idt[KEYBOARD_CALLS], ) // Keyboard Calls
-    // // SET_IDT_ENTRY(idt[RTC_CALLS], ); // RTC Calls
+    SET_IDT_ENTRY(idt[0], division_error); // Division Error
+    SET_IDT_ENTRY(idt[1], debug); // Debug
+    SET_IDT_ENTRY(idt[2], non_maskable_interrupt); // Non-maskable Interrupt
+    SET_IDT_ENTRY(idt[3], breakpoint); // Breakpoint
+    SET_IDT_ENTRY(idt[4], overflow); // Overflow
+    SET_IDT_ENTRY(idt[5], bound_range_exceeded); // Bound Range Exceeded
+    SET_IDT_ENTRY(idt[6], invalid_opcode); // Invalid Opcode
+    SET_IDT_ENTRY(idt[7], device_not_available); // Device Not Available
+    SET_IDT_ENTRY(idt[8], double_fault); // Double Fault
+    SET_IDT_ENTRY(idt[9], coprocessor_segment_overrun); // Coprocessor Segment Overrun
+    SET_IDT_ENTRY(idt[10], invalid_tss); // Invalid TSS
+    SET_IDT_ENTRY(idt[11], segment_not_present); // Segment Not Present
+    SET_IDT_ENTRY(idt[12], stack_segment_fault); // Stack-Segment Fault
+    SET_IDT_ENTRY(idt[13], general_protection_fault); // General Protection Fault
+    SET_IDT_ENTRY(idt[14], page_fault); // Page Fault
+    SET_IDT_ENTRY(idt[16], asm_floating_point_exception); // x87 Floating-Point Exception
+    SET_IDT_ENTRY(idt[17], alignment_check); // Alignment Check
+    SET_IDT_ENTRY(idt[18], machine_check); // Machine Check
+    SET_IDT_ENTRY(idt[19], simd_floating_point_exception); // SIMD Floating-Point Exception
+    SET_IDT_ENTRY(idt[SYSTEM_CALLS], system_call); // System Calls
+    SET_IDT_ENTRY(idt[KEYBOARD_CALLS], system_call); // Keyboard Calls
+    SET_IDT_ENTRY(idt[RTC_CALLS], system_call); // RTC Calls
 
-    lidt(idt_desc_ptr);
+    lidt(idt_desc_ptr); // load the idt
 }
