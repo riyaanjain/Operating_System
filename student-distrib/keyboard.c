f07979735137eed082099e2eb0c225f4f17dabd2
aryans5 <aryans5@illinois.edu>
2024-03-19 17:31:26 -0500

    Added function headers to keyboard.c

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -4,10 +4,24 @@ char ascii[58] = { 0x0, 0x0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '
                    'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 0x0, 0x0,'a', 's', 'd', 'f', 'g', 'h', 'j', 'k',
                    'l', ';', '\'', '`', 0x0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0x0, 0x0, 0x0, ' '};
 
+/* void init_keyboard()
+ *  Functionality: Initializes the keyboard interrupt
+ *  Arguments: None
+ *  Return: None
+ ***********************************************************************************
+ *  
+ */
 void init_keyboard() {
     enable_irq(keyboard_irq);
 }
 
+/* void keyboard_handler()
+ *  Functionality: Interrupt handler for keyboard events
+ *  Arguments: None
+ *  Return: None
+ ***********************************************************************************
+ *  
+ */
 void keyboard_handler() {
     cli();
     uint8_t c = inb(keyboard_data_port);
