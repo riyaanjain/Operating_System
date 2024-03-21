52c1007e67e827389ba997dbf38d7a7d81398a41
Kushal Chava <kchav5@illinois.edu>
2024-03-21 17:45:10 -0500

    Fixed exception handling

--- student-distrib/exception_handler.c
+++ student-distrib/exception_handler.c
@@ -3,28 +3,14 @@
 void exc_handler(uint32_t id, struct registers regs, uint32_t error_code) {
     // clear the screen
     clear();
-
-    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-    printf("\t\t\tException Synopsis:\n\n");
-
-    if (id < NUM_EXCEPTIONS) {
-        printf("\t\t\tException Raised: %s\n", os_exceptions[id]);
-        printf("\t\t\tEntry Number: %d\n\n", id);
+    printf("\n");
+    printf("Exception Synopsis:\n");
+    if (id != SYSTEM_CALLS) {
+        if(id < NUM_EXCEPTIONS) {
+            printf("Exception raised %s", os_exceptions[id]);
+        }
+    } else {
+        printf("System Call Exception");
     }
-
-
-    printf("\t\t\tTechnical Error Code Data:\n\n");
-    printf("\t\t\tError Code: %d\n\n", error_code);
-
-    printf("\t\t\tRegister Information:\n\n");
-    printf("\t\t\tEAX: %d\n", regs.eax);
-    printf("\t\t\tEBX: %d\n", regs.ebx);
-    printf("\t\t\tECX: %d\n", regs.ecx);
-    printf("\t\t\tEDX: %d\n", regs.edx);
-    printf("\t\t\tESI: %d\n", regs.esi);
-    printf("\t\t\tEDI: %d\n", regs.edi);
-    printf("\t\t\tESP: %d\n", regs.esp);
-    printf("\t\t\tEBP: %d\n", regs.ebp);
-
     while(1);
 }
